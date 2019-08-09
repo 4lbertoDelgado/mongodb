8592eba970c8d6892c52c198d3b913aba23be5c3
Delgado Quijandria Pierre Alberto <pdelgadoq@sunat.gob.pe>
2019-08-09 18:20:04 -0500

    Add Update method

--- M001_MONGODB_BASICS/C2_MongoDB_query_languaje_atlas.js
+++ M001_MONGODB_BASICS/C2_MongoDB_query_languaje_atlas.js
@@ -68,22 +68,68 @@ db.movieDetails.find({"rated": "PG", "awards.nominations": 10}).count()
 
 Filtrado de documentos Array
 ------------------------------
-Los elementos filtrados son respetando exactamente el orden indicado
-db.movieDetails.find({"actors": ["Jeff Bridges", "Tim Robbins"]}).pretty()
+Los elementos filtrados son respetando exactamente los consultados en el orden indicado
+db.movieDetails.find({"actors": ["Jeff Bridges", "Terrence Howard"]}).pretty()
+db.movieDetails.find({"writers": ["Ethan Coen", "Joel Coen"]}).count()
+
 
 Los elementos filtrados son los que contengan a Jeff Bridges en su array
-db.movieDetails.find({"actors": "Jeff Bridges"}).pretty()
+db.movieDetails.find({"actors": "Jeff Bridges"}).count()
+
+Los elementos filtrados son los que contengan a Jeff Bridges y Terrence Howard en su array
+Usamos el operador $all para obtener una matriz que contenga los elementos sin tener en cuenta el orden o que existan otros elementos en la matriz.
+db.movieDetails.find({"actors": { $all: ["Jeff Bridges" , "Terrence Howard"]}}, {"title": 1})
+db.movieDetails.find({"genres": "Family"}).count()
+
+Por ejemplo, la siguiente operación consulta todos los documentos donde la matriz dim_cm contiene al menos un elemento cuyo valor es mayor que 25.
+db.inventory.find( { dim_cm: { $gt: 25 } } )
+
+El siguiente ejemplo consulta para documentos donde la matriz dim_cm contiene elementos que en alguna combinación satisfacen las condiciones de consulta; por ejemplo, un elemento puede satisfacer la condición superior a 15 y otro elemento puede satisfacer la condición inferior a 20, o un solo elemento puede satisfacer ambas:
+db.inventory.find( { dim_cm: { $gt: 15, $lt: 20 } } )
+
+El siguiente ejemplo consulta para documentos donde la matriz dim_cm contiene al menos un elemento que es mayor que ($ gt) 22 y menor que ($ lt) 30. es decir usamos el operador $elemMatch para indicar que se deben satisfacer ambas.
+db.inventory.find( { dim_cm: { $elemMatch: { $gt: 22, $lt: 30 } } } )
 
 Los elementos filtrados son los que contienen a Jeff Bridges en la pocision 0 en su matriz
 db.movieDetails.find({"actors.0": "Jeff Bridges"}).pretty()
+db.movieDetails.find({"genres.1": "Western"}).count()
+
+El siguiente ejemplo consulta todos los documentos donde el segundo elemento en la matriz dim_cm es mayor que 25
+db.inventory.find( { "dim_cm.1": { $gt: 25 } } )
+
+Use el operador $ size para consultar matrices por número de elementos. Por ejemplo, lo siguiente selecciona documentos donde las etiquetas de matriz tienen 3 elementos
+db.inventory.find( { "tags": { $size: 3 } } )
 
 
 use 100YWeatherSmall
 db.collectionName.find({"wind.direction.angle": 290}).pretty()
 
+Metodo find
+-----------------------
+
+El metodo find devuelve un cursor
+Un corusor es esencialmente un puntero a la ubicacion actual
+Por lote el cursor en mogoshell devuelve los primeros 20 resultados
+En caso hubiera mas, mongo shell habilita una funcion getMore() para obtener el siguiente lote de 20 iteraciones
+
+Para ver los siguientes resultados en shell colocar it que es el acronimo de iterar
+it
+
+Proyecciones
+-------------------------
+Las Proyecciones reducen la sobrecarga y el procesamiento de la red limitando los campos que son devueltos
+Por defecto mongodb devuelve todos los documentos coincidentes con consultas 
+Se puede definir una proyeccion como el segundo argumento del metodo find
+
+Si deseo limitar mis documentos para que los resultados solo contengan el campo titulo puedo hacerlo agregando un segundo argumento al metodo find:
+Usamos el operador $all para obtener una matriz que contenga los elementos sin tener en cuenta el orden u otros elementos en la matriz.
+Aqui incluimos explicitamente el campo title cuando le pasamos 1, para excluirlo le pasamos 0
 
 
+db.movieDetails.find({"genres": { $all: ["Comedy" , "Western"]}}, {"title": 1})
+db.movieDetails.find({"genres": { $all: ["Comedy" , "Western"]}}, {"title": 1, "_id": 0})
 
+ 
 
 Insert
 -----------------
@@ -166,3 +212,32 @@ db.moviesScratch.insertMany(
         "ordered": false 
     }
 );
+
+
+UPDATE
+---------------------------
+
+updateOne
+Actualiza el primer documento que encuentra que conincida con el filtro
+La respuesta de updateOne nos indica la cantidad de documentos actualizados, para updateOne siempre sera 1
+1er parametro: {filter}
+2do parametro: {<$opcion> : {"key": "value"}}
+
+$set Toma un documento como argumento y actualiza el documento que coincide con el filtro. si el campo no existe lo crea, si existe lo actualiza
+db.movieDetails.updateOne({"title": "The Martian"}, {$set: {"poster": "https://www.imdb.com/title/tt3659388/mediaviewer/rm1391324160"}})
+
+$unset
+borra completamente el campo que le pasamos de un documento
+
+$min $max
+Nos permiten actualizar un campo basados en una comparacion	tomado como el minimo o el maximo de lso dos valores
+
+$inc
+incrementa el valor almacenado
+db.movieDetails.updateOne({"title": "The Martian"}, {$inc: {"tomato.reviews": 3, "tomato.userReviews": 25}})
+
+$addToSet
+Actualizara una matriz con nuevos valores, solo si el valor no esta contenido en la matriz
+
+$pop $pull
+Puedo sacar el primero o el ultimo elemento de una matriz
